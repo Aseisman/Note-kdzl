c3d62a9ef61fb9bde3bc765e4ce2c3eed2dc5abe
Cargo <876956898@qq.com>
2022-03-21 09:37:39 +0800

    存

---
+++ index.js
@@ -0,0 +1,12 @@
+// Ext.ComponentMgr.all
+
+// Ext.getCmp("sss").destroy()
+// var b= Ext.ComponentMgr.all.clone()
+// // 点击一下
+// // Ext.getCmp("sss").destroy()
+// // 再销毁
+// b.filter(v=>)
+const charTxt='a';
+let count =  2;
+const key = new Array(count).fill(charTxt).join("")+0.1;
+console.log(key);
